37b3ebd089380c697246e6492b6bd63bc9b0e251
Voda Ioan <83557961+vodaioan03@users.noreply.github.com>
2024-10-02 18:42:50 +0300

    Features: + topbar with features + dark/white mode theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
-
+import Topbar from './scenes/global/Topbar';
+import { ColorModeContext, useMode} from './theme';
+import { CssBaseline, ThemeProvider} from '@mui/material';
 function App() {
-  return (
-    <div className="app"></div>
+  const [theme, colorMode] = useMode();
+  return ( 
+    <ColorModeContext.Provider value = {colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <div className="app">
+            <main className="content">
+              <Topbar/>
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
